fb70c5df3356128ede5f6165f820ea882227df7c
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-01-16 18:37:28 -0300

    Inserido Home.js e feito alguns ajuste na responsividade

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,31 +6,35 @@ import rocketLogo from '../assets/images/rocketLogo.png';
 class NavBar extends Component {
     render() {
         return (
-            <div id='container-navbar'>
+            <>
+                <div id='container-navbar'>
 
-                <div id='logo'>
-                    <img src={rocketLogo} alt='Logo'/>
-                    <label>RocketDev</label>
+                    <div id='logo'>
+                        <img src={rocketLogo} alt='Logo' />
+                        <label>Rocket<span className='dev'>Dev</span></label>
+                    </div>
+
+                    <div className='control-menu'>
+
+                        <nav>
+
+                            <input type='checkbox' id='check-btn' />
+                            <label for='check-btn' className='btn-menu'>&#9776;</label>
+
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/">Sobre mim</a></li>
+                                <li><a href="/">Resumo</a></li>
+                                <li><a href="/">Conhecimentos</a></li>
+                                <li><a href="/">Contato</a></li>
+                            </ul>
+
+                        </nav>
+
+                    </div>
+                    
                 </div>
-
-                <div className='control-menu'>
-
-                    <nav>
-
-                        <input type='checkbox' id='check-btn' />
-                        <label for='check-btn' className='btn-menu'> &#9776;</label>
-
-                        <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/">Sobre mim</a></li>
-                            <li><a href="/">Resumo</a></li>
-                            <li><a href="/">Conhecimentos</a></li>
-                            <li><a href="/">Contato</a></li>
-                        </ul>
-
-                    </nav>
-                </div>
-            </div>
+            </>
         );
     }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 
-import NavBar from '../components/NavBar';
 import '../styles/pages/Home.css';
 
+import NavBar from '../components/NavBar';
+import templateHome from '../assets/images/templateHome.png';
+import rocketHome from '../assets/images/rocketLogo.png';
+
+
 class Home extends Component {
-    render(){
-        return(
-            <div>
+    render() {
+        return (
+            <>
                 <NavBar />
-            </div>
+
+                <div id='containerHome'>
+
+                    <div className='containerImagesHome'>
+                        <img className='formatImagesHome' src={templateHome} alt='templateHome' />
+                    </div>
+
+                    <div id='containerWelcomeFormat'>
+
+                        <div className='textWelcome'>
+                            <p>Olá, <br /> eu sou<span> Rafael Marcelo</span>,
+                            desenvolvedor <span className='fullstack'> fullStack</span>.</p>
+                        </div>
+
+                        <div className='btn-git'>
+                            <a href='https://github.com/rafaelmarcelo08' target='_blank' rel="noreferrer">github</a>
+                        </div>
+
+                    </div>
+
+                    <div className='containerImagesHome second'>
+                        <img className='formatImagesHome ' src={rocketHome} alt='rocketHome' />
+                    </div>
+
+                </div>
+
+
+
+            </>
         );
     }
 }
