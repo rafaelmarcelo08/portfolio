4fedf3dd5a30e697229e81ca2e9e2dcf28947d97
Rafael Marcelo <rafael.2018.ads@gmail.com>
2024-11-15 20:52:25 -0300

    ajustes

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
-
+  public faGithub = faGithub;
+  public faLinkedin = faLinkedin;
+  public faFile = faFile;
 }
--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { NgxTypedJsModule } from 'ngx-typed2';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { ProfileComponent } from './profile.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [SharedModule],
+  imports: [SharedModule, NgxTypedJsModule, FontAwesomeModule],
   exports: [ProfileComponent],
 })
 export class ProfileModule {}
