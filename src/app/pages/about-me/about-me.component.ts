0d323c4bd695b2ea0a44beb460d544f6b50ee04d
Rafael Marcelo <rafael.2018.ads@gmail.com>
2024-11-17 12:09:37 -0300

    add botao certificado

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faCertificate } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent {
-
+  public faCertificate = faCertificate;
 }
